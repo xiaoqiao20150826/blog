6e93a3d59f2ab58b0c5540b77506a77555f68bfe
Philip lanphar <pjlanphar@gmail.com>
2015-08-02 21:14:18 -0700

    Added a drop down to the Navbar

--- public/javascript/controllers/HomeController.js
+++ public/javascript/controllers/HomeController.js
@@ -3,12 +3,13 @@
 	angular.module('app')
 	.controller('HomeController', HomeController);
 
-	HomeController.$inject = ["HomeFactory", "$state"];
+	HomeController.$inject = ["HomeFactory", "UserFactory", "$state"];
 
-	function HomeController(HomeFactory, $state) {
+	function HomeController(HomeFactory, UserFactory, $state) {
 		var vm = this;
 		vm.pageTitle = 'myBlog!';
 		vm.listBlog = HomeFactory.listBlog;
+		vm.userName = UserFactory.status.username;
 
 		vm.deleteBlog = function (blog) {
 			HomeFactory.deleteBlog(blog).then(function (){
